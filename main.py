e5059aee6df96371a2014312456c2b3c7614856e
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-11-10 18:26:01 +0200

    Login and Password variables are created

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 print("Denys, pay attention!!!") # Денис, приділи цьому увагу!!!
 print("Цей рядок для того, щоб пояснити git push")
 
+LOGIN = "LOGIN"
+PASSWORD = "PASS"
+
 def input_cred():
     login = input(">>>") 
     password = input(">>>")
     
-    if login != "login" or password != "password":
+    if login != LOGIN or password != PASSWORD:
         print("Wrong login or password")
-    elif login == "login" and password == "password":
+    elif login == LOGIN and password == PASSWORD:
         print("Success")
 
 input_cred()
